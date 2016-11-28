7ecfc128b33e19f8b24c0686f102f5a1b085b78f
Derek Chang <derek@gigwalk.com>
2016-11-28 13:46:28 -0800

    SVM mini project modified:   svm_author_id.py modified:   ../naive_bayes/nb_author_id.py

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,19 +12,32 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
-
+from sklearn import svm
+from sklearn.metrics import accuracy_score
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
-
+# smaller data set
+# features_train = features_train[:len(features_train)/100] 
+# labels_train = labels_train[:len(labels_train)/100] 
 
 
 #########################################################
 ### your code goes here ###
-
+t0 = time()
+# clf = svm.SVC(kernel='linear')
+clf = svm.SVC(kernel='rbf', C=10000)
+clf.fit(features_train, labels_train)
+print "training time: {}s:".format(round(time()-t0, 3))
+t0 = time()
+pred = clf.predict(features_test)
+print "prediction time: {}s".format(round(time()-t0, 3))
+print accuracy_score(pred, labels_test)
+print pred[10], pred[26], pred[50]
+count = 0
+chris = len([i for i in pred if i == 1])
+print chris
 #########################################################
-
-
